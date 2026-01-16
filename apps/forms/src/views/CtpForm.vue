445d5efc60e414bccea60764269b37bc28e0727e
derekm <derek@railboxconsulting.com>
2026-01-16 10:23:34 -0700

    make the family subdomain value in the redirect url to CTP Leaders dynamic. Add _redirects to leaderboard app public folder

--- apps/forms/src/views/CtpForm.vue
+++ apps/forms/src/views/CtpForm.vue
@@ -78,7 +78,7 @@ async function submit() {
 
     // 🔁 Redirect to generic CTP leaders
     setTimeout(() => {
-      window.location.href = "https://fore-skore.com/ctp";
+      window.location.href = `https://${family.value}.fore-skore.com/ctp`;
     }, 300);
   } catch (e) {
     err.value = String(e?.message ?? e);
